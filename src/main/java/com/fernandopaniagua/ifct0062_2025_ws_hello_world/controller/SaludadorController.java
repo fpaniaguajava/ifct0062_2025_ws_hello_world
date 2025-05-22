c7032fd1b16aa1c3900c1b8bcfe12a0104120586
fpaniaguajava <fernando.paniagua.java@gmail.com>
2025-05-22 18:35:35 +0200

    Cambio de inglés a español

--- src/main/java/com/fernandopaniagua/ifct0062_2025_ws_hello_world/controller/SaludadorController.java
+++ src/main/java/com/fernandopaniagua/ifct0062_2025_ws_hello_world/controller/SaludadorController.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class SaludadorController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hola, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/saludo")
